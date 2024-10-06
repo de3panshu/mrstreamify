afd09f3c1f0b7276f6aec70b532137e6c27e12d9
Deepanshu <sahu.deeepanshu.united@gmail.com>
2024-10-06 13:20:41 +0530

    intial setup done

---
+++ src/main/java/com/hd/api/mrstreamify/MrstreamifyApplication.java
@@ -0,0 +1,13 @@
+package com.hd.api.mrstreamify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrstreamifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrstreamifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hd/api/mrstreamify/MrstreamifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hd.api.mrstreamify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrstreamifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
